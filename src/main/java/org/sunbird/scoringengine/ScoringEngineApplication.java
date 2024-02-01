71419c93e4b46b56008b548c12a74a4bac795ab9
samynathan <samynathan.shanmugam@tarento.com>
2024-02-01 14:45:11 +0530

    inside scoring controller class add comments apis

--- src/main/java/org/sunbird/scoringengine/ScoringEngineApplication.java
+++ src/main/java/org/sunbird/scoringengine/ScoringEngineApplication.java
@@ -4,11 +4,9 @@ package org.sunbird.scoringengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.sunbird.scoringengine.comments.*", "org.sunbird.scoringengine.*"})
 public class ScoringEngineApplication {
 
     public static void main(String[] args) {
