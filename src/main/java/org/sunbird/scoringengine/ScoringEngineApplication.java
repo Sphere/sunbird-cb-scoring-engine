3a4cf408c199ef5fdc702beb922f563304009759
samynathan <samynathan.shanmugam@tarento.com>
2024-02-01 13:03:49 +0530

    annotation.Configuration added

--- src/main/java/org/sunbird/scoringengine/ScoringEngineApplication.java
+++ src/main/java/org/sunbird/scoringengine/ScoringEngineApplication.java
@@ -4,13 +4,17 @@ package org.sunbird.scoringengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@ComponentScan(basePackages = "org.sunbird.scoringengine.*")
+@Configuration
 @SpringBootApplication
 public class ScoringEngineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScoringEngineApplication.class, args);
-
+        
     }
 
 }
